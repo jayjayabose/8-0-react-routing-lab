d80881d0237517ceb9e4bd6c2abcc0490478c14f
Jay Jayabose <jayabose@gmail.com>
2022-06-30 07:14:23 -0400

    extras: 404 and owners page

---
+++ src/components/staff/NewStaff.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class NewStaff extends React.Component {
+    render () {
+        return (
+            <h3>Add New Staff Member</h3>
+        )
+    }    
+}
+
+export default NewStaff;
